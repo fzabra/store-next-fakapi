cc104b2b09bbcc37d109840e2b0edefffec0f372
FZ <fzabra@gmail.com>
2024-10-17 18:54:49 -0300

    [feat] Product detail page

--- api/products.ts
+++ api/products.ts
@@ -1,5 +1,6 @@
-// lib/products.ts
+import { ReactNode } from "react";
 export interface Product {
+    description: ReactNode;
     category: string;
     id: number;
     title: string;
@@ -18,3 +19,27 @@ export interface Product {
     return products;
   };
   
+export interface ProductDetail {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+}
+
+export const fetchProductById = async (id: string | string[] | undefined): Promise<ProductDetail | null> => {
+  if (!id) return null;
+
+  try {
+    const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+    if (!res.ok) {
+      throw new Error('Product not found');
+    }
+    const product: ProductDetail = await res.json();
+    return product;
+  } catch (error) {
+    console.error('Error fetching product:', error);
+    return null;
+  }
+};
